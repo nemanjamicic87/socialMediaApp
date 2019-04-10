a06e01f9f04e2da28c9e2df9991fb36de48c79b5
Nemanja <nmicic987@gmail.com>
2019-04-10 21:37:30 +0200

    validation added

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,10 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
+//Load Input Validation
+const validationRegisterInput = require('../../validation/register');
 
 //Load User model
 const User = require('../../models/User');
@@ -17,10 +21,16 @@ router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
 // @desc   Register user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validationRegisterInput(req.body);
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   //use mongose to find if email exist
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email allready exists' });
+      errors.email = 'Email allready exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //size
@@ -87,5 +97,19 @@ router.post('/login', (req, res) => {
     });
   });
 });
+// @route  GET api/users/current
+// @desc   Returning current user
+// @access Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
